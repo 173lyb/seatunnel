d7b5b3c5be126e55559583e1e5b8ef9bedd4e9cb
lyb <1733839298@qq.com>
2024-08-20 10:44:29 +0800

    [feature] [transform] 新增 时间转换格式，sm3加密

---
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/sql/zeta/functions/udf/Sm3Encrypt.java
@@ -0,0 +1,48 @@
+package org.apache.seatunnel.transform.sql.zeta.functions.udf;
+
+import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.transform.sql.zeta.ZetaUDF;
+
+import org.bouncycastle.crypto.digests.SM3Digest;
+import org.bouncycastle.util.encoders.Hex;
+
+import com.google.auto.service.AutoService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/** @Author 熊呈 @Mail xiongcheng@xugudb.com @Date 2024/8/9 11:23 */
+@AutoService(ZetaUDF.class)
+public class Sm3Encrypt implements ZetaUDF {
+
+    @Override
+    public String functionName() {
+        return "Sm3Encrypt";
+    }
+
+    @Override
+    public SeaTunnelDataType<?> resultType(List<SeaTunnelDataType<?>> list) {
+        return BasicType.STRING_TYPE;
+    }
+
+    @Override
+    public Object evaluate(List<Object> list) {
+        String inputString;
+        if (list.size() == 1) {
+            inputString = list.get(0).toString();
+        } else {
+            inputString = list.stream().map(Object::toString).collect(Collectors.joining(","));
+        }
+        return sm3EncryptCore(inputString);
+    }
+
+    private String sm3EncryptCore(String input) {
+        SM3Digest digest = new SM3Digest();
+        byte[] inputBytes = input.getBytes();
+        digest.update(inputBytes, 0, inputBytes.length);
+        byte[] hash = new byte[digest.getDigestSize()];
+        digest.doFinal(hash, 0);
+        return Hex.toHexString(hash);
+    }
+}
---
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/sql/zeta/functions/udf/TimeTrans.java
@@ -0,0 +1,41 @@
+package org.apache.seatunnel.transform.sql.zeta.functions.udf;
+
+import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.common.utils.SeaTunnelException;
+import org.apache.seatunnel.transform.sql.zeta.ZetaUDF;
+
+import com.google.auto.service.AutoService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+/** @Author 熊呈 @Mail xiongcheng@xugudb.com @Date 2024/8/9 15:17 */
+@AutoService(ZetaUDF.class)
+public class TimeTrans implements ZetaUDF {
+    @Override
+    public String functionName() {
+        return "TimeTrans";
+    }
+
+    @Override
+    public SeaTunnelDataType<?> resultType(List<SeaTunnelDataType<?>> list) {
+        return BasicType.STRING_TYPE;
+    }
+
+    @Override
+    public Object evaluate(List<Object> list) {
+        String timeValue = list.get(0).toString();
+        timeValue = timeValue.replace("T", " ");
+        String format = list.get(1).toString();
+        try {
+            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern(format);
+            DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            LocalDateTime dateTime = LocalDateTime.parse(timeValue, inputFormatter);
+            return dateTime.format(outputFormatter);
+        } catch (Exception e) {
+            throw new SeaTunnelException("转换日期失败" + e);
+        }
+    }
+}
