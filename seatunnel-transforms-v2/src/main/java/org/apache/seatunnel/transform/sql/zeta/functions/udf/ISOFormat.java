62cfbf1f0c9f87ece5d5f2e41f7dd7b8f53e7cf6
lyb <1733839298@qq.com>
2024-08-21 14:08:29 +0800

    [feature] [transform] isoFormat 格式化检查

--- seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/sql/zeta/functions/udf/ISOFormat.java
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/sql/zeta/functions/udf/ISOFormat.java
@@ -1,10 +1,11 @@
 package org.apache.seatunnel.transform.sql.zeta.functions.udf;
 
-import com.google.auto.service.AutoService;
-import com.google.common.base.Strings;
 import org.apache.seatunnel.api.table.type.BasicType;
 import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
 import org.apache.seatunnel.transform.sql.zeta.ZetaUDF;
+
+import com.google.auto.service.AutoService;
+import com.google.common.base.Strings;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -32,16 +33,22 @@ public class ISOFormat implements ZetaUDF {
     public Object evaluate(List<Object> args) {
         String dateStr = (String) args.get(0);
         String NewFormat = (String) args.get(1);
-        if (dateStr == null) {return null;}
+        if (dateStr == null) {
+            return null;
+        }
 
         DateTimeFormatter parser1 = DateTimeFormatter.ISO_DATE_TIME;
 
         // Create a new formatter for the non-colon time zone format
-        DateTimeFormatter parser2 = new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-                .toFormatter();
+        DateTimeFormatter parser2 =
+                new DateTimeFormatterBuilder()
+                        .appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+                        .toFormatter();
 
-        DateTimeFormatter formatter = Strings.isNullOrEmpty(NewFormat) ?
-                DateTimeFormatter.ofPattern(DEFAULT_FORMAT) : DateTimeFormatter.ofPattern(NewFormat);
+        DateTimeFormatter formatter =
+                Strings.isNullOrEmpty(NewFormat)
+                        ? DateTimeFormatter.ofPattern(DEFAULT_FORMAT)
+                        : DateTimeFormatter.ofPattern(NewFormat);
         try {
             ZonedDateTime date = ZonedDateTime.parse(dateStr, parser1);
             return date.format(formatter).toString();
@@ -59,22 +66,30 @@ public class ISOFormat implements ZetaUDF {
     public Object evaluate2(List<Object> args) {
         String dateStr = (String) args.get(0);
         String NewFormat = (String) args.get(1);
-        if (dateStr == null) {return null;}
+        if (dateStr == null) {
+            return null;
+        }
 
-        DateTimeFormatter dtfInput = new DateTimeFormatterBuilder()
-                .appendPattern("[yyyy-MM-dd'T'HH:mm:ss.SSSX][yyyy-MM-dd'T'HH:mm:ssX][yyyy-MM-dd'T'HH:mm:ssXXX]")
-                .toFormatter();
-        DateTimeFormatter formatter = Strings.isNullOrEmpty(NewFormat) ?
-                DateTimeFormatter.ofPattern(DEFAULT_FORMAT) : DateTimeFormatter.ofPattern(NewFormat);
+        DateTimeFormatter dtfInput =
+                new DateTimeFormatterBuilder()
+                        .appendPattern(
+                                "[yyyy-MM-dd'T'HH:mm:ss.SSSX][yyyy-MM-dd'T'HH:mm:ssX][yyyy-MM-dd'T'HH:mm:ssXXX]")
+                        .toFormatter();
+        DateTimeFormatter formatter =
+                Strings.isNullOrEmpty(NewFormat)
+                        ? DateTimeFormatter.ofPattern(DEFAULT_FORMAT)
+                        : DateTimeFormatter.ofPattern(NewFormat);
         try {
-            TemporalAccessor ta = dtfInput.parseBest(dateStr, ZonedDateTime::from, LocalDateTime::from, LocalDate::from);
-            if (ta instanceof ZonedDateTime){
+            TemporalAccessor ta =
+                    dtfInput.parseBest(
+                            dateStr, ZonedDateTime::from, LocalDateTime::from, LocalDate::from);
+            if (ta instanceof ZonedDateTime) {
                 ZonedDateTime zdt = ZonedDateTime.from(ta);
                 return zdt.format(formatter).toString();
-            } else if (ta instanceof LocalDateTime){
+            } else if (ta instanceof LocalDateTime) {
                 LocalDateTime ldt = LocalDateTime.from(ta);
                 return ldt.format(formatter).toString();
-            } else if (ta instanceof LocalDate){
+            } else if (ta instanceof LocalDate) {
                 LocalDate ld = LocalDate.from(ta);
                 return ld.format(formatter).toString();
             }
