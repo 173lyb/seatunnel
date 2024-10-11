c5560ba8f1e952422ca4802d29e171a9fd0d9c55
lyb <1733839298@qq.com>
2024-10-11 10:44:13 +0800

    [feature] [jdbc-Connector-2.3.8] 适配新版querySQLResultExists方法改动

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/xugu/XuguCatalog.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/xugu/XuguCatalog.java
@@ -25,6 +25,7 @@ import org.apache.seatunnel.api.table.catalog.exception.CatalogException;
 import org.apache.seatunnel.api.table.converter.BasicTypeDefine;
 import org.apache.seatunnel.common.exception.SeaTunnelRuntimeException;
 import org.apache.seatunnel.common.utils.JdbcUrlUtil;
+import org.apache.seatunnel.common.utils.SeaTunnelException;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.catalog.AbstractJdbcCatalog;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.catalog.utils.CatalogUtils;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.xugu.XuguTypeConverter;
@@ -232,6 +233,8 @@ public class XuguCatalog extends AbstractJdbcCatalog {
                 return listDatabases().contains(databaseName.toUpperCase());
             }
             throw e;
+        } catch (SQLException e) {
+            throw new SeaTunnelException("查询kingbase数据库列表异常", e);
         }
     }
 
